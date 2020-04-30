c41298c5ac3ab4a13019af383993328ef0cedfaf
Mikami <cordion_nap@yahoo.co.jp>
2020-05-01 04:10:23 +0900

    Creating Book model and transitioning to /books, etc.

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,31 @@
+class BooksController < ApplicationController
+  def index
+    getAllBooks
+    new()
+  end
+
+  def show
+  end
+
+  def getAllBooks
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+  end
+
+  def edit
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  """
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  """
+  resources :books
   root :to => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
